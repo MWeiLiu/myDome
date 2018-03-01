33e0fd0ac51429e16ec4dbd84c84f3115dbcafb7
刘威 <872496024@qq.com>
2018-03-01 12:51:20 +0800

    移除一些私密信息

--- python/python1/WebProject1/class2.py
+++ python/python1/WebProject1/class2.py
@@ -4,7 +4,7 @@
 import class1
 #from class1 import age
 
-#print(class1.myClass.myDef('刘威', '22'));
+#print(class1.myClass.myDef('大侠', '22'));
 
 #print(age(22));
 
--- python/python1/WebProject1/smtplib.py
+++
@@ -1,9 +0,0 @@
-
-import smtplib
-
-from_addr = '872496024@qq.com';
-to_addrs = '2742306186@qq.com';
-msg = 'python';
-
-SMTP.sendmail(from_addr, to_addrs, msg);
-#pip install
